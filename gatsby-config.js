7f852ec3d84861131594b0f73f204f48d438037e
McSteve7 <stephenajiboye@yahoo.com>
2021-02-24 08:20:14 +0100

    few changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,14 +31,6 @@ module.exports = {
   plugins: [
 
     {
-      resolve: 'gatsby-source-contentful',
-      options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      },
-    },
-
-    {
       resolve: "@narative/gatsby-theme-novela",
       options: {
         contentPosts: "content/posts",
