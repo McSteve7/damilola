214228d8c44b014287b59e39656100ca79f341ea
Damilola Ajiboye <34410515+McSteve7@users.noreply.github.com>
2023-12-23 11:22:51 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
       },
       {
         name: `dribbble`,
-        url: `https://dribbble.com/DamilolaA`,
+        url: `https://dribbble.com/DamilolaA/likes`,
       },
     ],
   },
